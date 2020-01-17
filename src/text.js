ee116a2494e20a39eeaa3c322df8067b968a7685
Sicheng886 <58097617+Sicheng886@users.noreply.github.com>
2020-01-17 12:04:27 +1000

    init project

---
+++ src/text.js
@@ -0,0 +1,19 @@
+export const enText = {
+  title: 'Happy Vib!',
+  preset: 'Preset',
+  goBtn: 'Go Vib!',
+  continues: 'Continues Vib!',
+  stop: 'Stop vib!',
+  intro: 'Input nums (million seconds) and separate by commas',
+  alert: 'Your device do not support vib'
+};
+
+export const chText = {
+  title: '快乐震动!',
+  preset: '预设',
+  goBtn: '开始震动',
+  continues: '持续狂震!',
+  stop: '停止狂震!',
+  intro: '输入数字(毫秒),以逗号分隔',
+  alter: '您的设备不支持此功能'
+};

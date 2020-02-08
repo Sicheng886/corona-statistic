0ff6f4660dc508f555ba8b10ee9556fd61f2e0c6
Sicheng886 <58097617+Sicheng886@users.noreply.github.com>
2020-02-08 12:41:52 +1000

    restore from chaos

--- src/index.js
+++ src/index.js
@@ -1,94 +1,18 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './component/App/App';
+import { Provider } from './Context';
+import * as serviceWorker from './serviceWorker';
 
-import vibType from './vibType.json';
-import { enText, chText } from './text';
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
-const text = navigator.language === 'zh-CN' ? chText : enText;
-
-const VibForm = () => {
-  const [vibVal, setVibVal] = useState('1000');
-  const [goContinue, setGoContinue] = useState(false);
-
-  const continuesVib = useRef();
-
-  useEffect(() => {
-    if (!goContinue) {
-      clearInterval(continuesVib.current);
-    }
-    if (goContinue) {
-      continuesVib.current = setInterval(() => {
-        if (navigator.vibrate) {
-          navigator.vibrate(1000);
-        } else {
-          alert(text.alert);
-        }
-        console.log('called');
-      }, 1000);
-    }
-  });
-
-  return (
-    <form>
-      <label>{text.preset}</label>
-      <div className='presets'>
-        {vibType.map((item, index) => {
-          return (
-            <button
-              key={index}
-              onClick={e => {
-                e.preventDefault();
-                setVibVal(item);
-              }}
-            >
-              {text.preset} {index + 1}
-            </button>
-          );
-        })}
-      </div>
-      <label>{text.intro}</label>
-      <input
-        type='text'
-        value={vibVal}
-        onChange={e => {
-          setVibVal(e.target.value);
-        }}
-      />
-      <button
-        name='btn'
-        type='button'
-        onClick={e => {
-          e.preventDefault();
-          if (navigator.vibrate) {
-            navigator.vibrate(vibVal.split(','));
-          } else {
-            alert(text.alert);
-          }
-        }}
-      >
-        {text.goBtn}
-      </button>
-      <div>
-        <button type='button' onClick={() => setGoContinue(true)}>
-          {text.continues}
-        </button>
-        <button type='button' onClick={() => setGoContinue(false)}>
-          {text.stop}
-        </button>
-      </div>
-    </form>
-  );
-};
-
-const Main = () => {
-  return (
-    <div className='container'>
-      <h1>{text.title}</h1>
-      <VibForm />
-      <p>Made by Wally, 2020</p>
-    </div>
-  );
-};
-
-ReactDOM.render(<Main />, document.getElementById('root'));
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register();
--- src/text.js
+++ src/text.js
@@ -1,19 +1,33 @@
 export const enText = {
-  title: 'Happy Vib!',
-  preset: 'Preset',
-  goBtn: 'Go Vib!',
-  continues: 'Continues Vib!',
-  stop: 'Stop vib!',
-  intro: 'Input nums (million seconds) and separate by commas',
-  alert: 'Your device do not support vib'
+  title: 'Current Coronavirus Statistics',
+  update: 'Last Update:',
+  selectArea: 'Select Area: ',
+  global: 'Global',
+  ch: 'China',
+  oversea: 'Overseas',
+  Confirmed: ' Confirmed',
+  Recovered: 'Recovered',
+  Deaths: 'Deaths',
+  Mainland_China: 'Mainland China',
+  Other_Locations: 'Other Locations',
+  datasource: 'Data source: '
 };
 
 export const chText = {
-  title: '快乐震动!',
-  preset: '预设',
-  goBtn: '开始震动',
-  continues: '持续狂震!',
-  stop: '停止狂震!',
-  intro: '输入数字(毫秒),以逗号分隔',
-  alter: '您的设备不支持此功能'
+  title: '实时疫情统计',
+  update: '最近更新',
+  selectArea: '选择区域: ',
+  global: '全球',
+  ch: '中国',
+  oversea: '海外',
+  Confirmed: '确诊',
+  Recovered: '康复',
+  Deaths: '死亡',
+  Mainland_China: '中国大陆',
+  Other_Locations: '其它地区',
+  datasource: '数据来源: '
 };
+
+const text = navigator.language === 'zh-CN' ? chText : enText;
+
+export default text;

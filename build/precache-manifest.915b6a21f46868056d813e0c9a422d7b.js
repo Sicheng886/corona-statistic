1dd8b3bb6a4a4a9fcfe42aadd3eaa2d481278773
Sicheng886 <58097617+Sicheng886@users.noreply.github.com>
2020-02-12 17:49:36 +1000

    trying to deploy to heroku

---
+++ build/precache-manifest.915b6a21f46868056d813e0c9a422d7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba81debb896f8a497a30a2df20e1c18f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c42dd086ea6f38e8cc57",
+    "url": "./static/css/main.85d554a5.chunk.css"
+  },
+  {
+    "revision": "10ccffcf1d2440ade2f6",
+    "url": "./static/js/2.23269985.chunk.js"
+  },
+  {
+    "revision": "490b6be34dc66e298cdb52e78b4e3aed",
+    "url": "./static/js/2.23269985.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c42dd086ea6f38e8cc57",
+    "url": "./static/js/main.762ce4f4.chunk.js"
+  },
+  {
+    "revision": "3b5d3b5309432bdc8255",
+    "url": "./static/js/runtime-main.99f90039.js"
+  }
+]);
